9397afa188603a0cb1c42c0e1ca8390b8a5b6d5d
Koudocko <tyler@pcquest.ca>
2023-09-10 21:26:13 -0400

    Added wehr6772_l06.

---
+++ Y1T1 - 2023/CP104/Labs/wehr6772_l06/functions.py
@@ -0,0 +1,156 @@
+def sum_all(start, finish, increment):
+    """
+    -------------------------------------------------------
+    Sums and returns all numbers from start to finish (inclusive)
+    by increment.
+    Use: total = sum_all(start, finish, increment)
+    -------------------------------------------------------
+    Parameters:
+        start - an integer (int > 0)
+        finish - an integer (int >= start)
+        increment - an integer (int > 0)
+    Returns:
+        total - sum of all numbers from start to
+            finish by increment (int)
+    ------------------------------------------------------
+    """
+    
+    # Use range in for loop to obtain nums to be summed 
+    total = 0
+    for num in range(start, finish + 1, increment):
+        total += num
+
+    return total
+
+def draw_hollow_triangle(width, char):
+    """
+    -------------------------------------------------------
+    Prints a hollow triangle of width characters using
+    the char character.
+    Use: draw_hollow_triangle(width, char)
+    -------------------------------------------------------
+    Parameters:
+        width - number of characters wide (int > 0)
+        char - the character to draw with (str, len() == 1)
+    Returns:
+        None
+    ------------------------------------------------------
+    """
+    
+    for num in range(0, width):
+        if num == 0: # Top of triange edge case
+            print(char)
+        elif num == width - 1: # Every other row
+            print(f"{'':#^{width}s}")
+        else: # Bottom of triange edge case
+            print(f"#{'#':>{num}s}")
+
+    return
+
+
+
+def retirement(age, salary, increase):
+    """
+    -------------------------------------------------------
+    Calculates a prints a table of how much a worker earns
+    between age and retirement at 65.
+    Use: retirement(age, salary, increase)
+    -------------------------------------------------------
+    Parameters:
+        age - worker's current age (int > 0)
+        salary - worker's current salary (float > 0)
+        increase - percent increase in salary per year (float >= 0)
+    Returns:
+        None
+    ------------------------------------------------------
+    """
+
+    # Chart width constants
+    WIDTH = 18
+    SALARY_WIDTH = WIDTH - 3
+
+    # Display headers
+    print(f"Age{'Salary':>{SALARY_WIDTH}s}") 
+    print(f"{'':-^{WIDTH}s}")
+
+    for year in range(age, 66):
+        # Dynamic spacing size
+        ROW_WIDTH = WIDTH - len(str(year))
+
+        # Display the row values of age and salary
+        print(f"{year}{salary:>{ROW_WIDTH},.2f}")
+        # Increase salary
+        salary *= 1 + (increase / 100)
+    
+    return
+
+def lumber(b_min, b_max, b_inc, h_min, h_max, h_inc):
+    """
+    -------------------------------------------------------
+    Create a table of the engineering properties of lumber.
+    Given the base and height of a piece of lumber in inches,
+    different properties of a piece of lumber are calculated as:
+        cross-sectional area = base × height
+        moment of inertia = base × height^3 / 12
+        section modulus = base × height^2 / 6
+    Use: lumber(b_min, b_max, b_inc, h_min, h_max, h_inc)
+    -------------------------------------------------------
+    Parameters:
+        b_min - minimum value of base (int > 0)
+        b_max - maximum value of base (int > b_min)
+        b_inc - increment in base value (int > 0)
+        h_min - minimum value of height (int > 0)
+        h_max - maximum value of height (int > h_min)
+        h_inc - increment in height value (int > 0)
+    Returns:
+        None
+    -------------------------------------------------------
+    """
+
+    # Display headers
+    print(f"{'Cross-Sectional':>29s}  Moment of  Section")
+    print(f"Base  Height  Area{'Inertia':>20s}    Modulus")
+    print(f"{'':-^49s}")
+
+    # Nested for loops, heights as the inner and bases as the outer
+    for base in range(b_min, b_max + 1, b_inc):
+        for height in range(h_min, h_max + 1, h_inc):
+            # Compute properties of lumber
+            cross_sectional_area = base * height
+            moment_of_inertia = base * height ** 3 / 12
+            section_modulus = base * height ** 2 / 6
+
+            # Display row with lumber properties data
+            print(f"{base:>4d}  X{height:>5d} {cross_sectional_area:>16.2f} {moment_of_inertia:>10.2f} {section_modulus:>8.2f}")
+
+    return
+
+def ia_hours(ia_count):
+    """
+    -------------------------------------------------------
+    Calculates the total number of hours that IAs (Instructional
+    Assistants) work over a 6 week period by asking for the hours
+    for each IA per week.
+    Use: total_hours = ia_hours(ia_count)
+    -------------------------------------------------------
+    Parameters:
+        ia_count - number of IAs (int > 0)
+    Returns:
+        total_hours - hours worked by all IAs (float)
+    ------------------------------------------------------
+    """
+
+    # Number of weeks IAs work constant
+    NUM_WEEKS = 6
+    
+    # Iterate from 1 to NUM_WEEKS (inclusive)
+    total_hours = 0
+    for week in range(1, NUM_WEEKS + 1):
+        print(f"Week {week}")
+
+        # Iterate over input for each IA in the week
+        for ia in range(1, ia_count + 1):
+            hours = float(input(f"  Marking hours for IA {ia}: "))
+            total_hours += hours
+    
+    return total_hours
---
+++ Y1T1 - 2023/CP104/Labs/wehr6772_l06/t03.py
@@ -0,0 +1,15 @@
+"""
+-------------------------------------------------------
+Sums all numbers in range with respect to increment
+-------------------------------------------------------
+Author:  Tyler Wehrle
+ID:      169056772
+Email:   wehr6772@mylaurier.ca
+__updated__ = "2023-09-09"
+-------------------------------------------------------
+"""
+
+from functions import sum_all
+
+# Call implemented sum_all function
+total = sum_all(5, 10, 1)
