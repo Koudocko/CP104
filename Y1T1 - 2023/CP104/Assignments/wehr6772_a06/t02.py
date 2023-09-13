f11a3a3abbcb1801fb99a375c87d967cdb896eb1
Koudocko <tyler@pcquest.ca>
2023-09-13 12:15:20 -0400

    Added CP104 wehr6772_a06.

---
+++ Y1T1 - 2023/CP104/Assignments/wehr6772_a06/t02.py
@@ -0,0 +1,21 @@
+"""
+-------------------------------------------------------
+Detects whether an input number is prime.
+-------------------------------------------------------
+Author:  Tyler Wehrle
+ID:      169056772
+Email:   wehr6772@mylaurier.ca
+__updated__ = "2023-09-12"
+-------------------------------------------------------
+"""
+
+# Imports
+from functions import detect_prime
+
+# Get number to be tested as prime
+number = int(input("Input number: "))
+
+# Display whether number is a prime number
+prime = detect_prime(number)
+print(f"detect_prime({number}) -> {prime}")
+
